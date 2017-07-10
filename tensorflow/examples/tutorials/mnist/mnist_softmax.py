40e348b392fe900adc3ae7b687880c3fa99653ae
saoriiiii <saori.shibuta@gmail.com>
2017-07-11 01:45:14 +0900

    Update mnist_softmax.py

--- tensorflow/examples/tutorials/mnist/mnist_softmax.py
+++ tensorflow/examples/tutorials/mnist/mnist_softmax.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+# Add: 2017/07/11
+# Purpose:leaving comments on code to get the idea of softmax function
+# ==============================================================================
 
 """A very simple MNIST classifier.
 
